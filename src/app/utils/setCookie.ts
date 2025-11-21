856394eb2d050b89ae895de29909e194a905314b
Sakib Islam <your-email@example.com>
2025-11-21 14:34:10 +0600

    asdf

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -1,23 +1,21 @@
 import { Response } from "express";
 import { envVariable } from "../config/env.config";
 
-export interface AuthTokens {
-    accessToken?: string;
-    refreshToken?: string;
-}
+export const setAuthCookies = (res: Response, accessToken: string, refreshToken: string) => {
+  const isProduction = process.env.NODE_ENV === "production";
+  const secureFlag = isProduction; 
+console.log(secureFlag)
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: true,  
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
 
-export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
-    if (tokenInfo.accessToken) {
-        res.cookie("accessToken", tokenInfo.accessToken, {
-            httpOnly: envVariable.NODE_ENV === "development",
-            secure: false
-        })
-    }
-
-    if (tokenInfo.refreshToken) {
-        res.cookie("refreshToken", tokenInfo.refreshToken, {
-            httpOnly: true,
-            secure: false,
-        })
-    }
-}
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,  
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
